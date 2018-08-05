a7e7a576207902ea3703c21d7d01681b0ce6fc94
OwO <41247578+youKnowOwO@users.noreply.github.com>
2018-08-05 17:05:50 +0700

    pun 😏

---
+++ commands/fun/pun.js
@@ -0,0 +1,22 @@
+exports.run = async (client, msg, args) => {
+	try{
+		const { body } = await client.snek.get('https://icanhazdadjoke.com/')
+		.set('Accept', 'application/json');
+		return msg.channel.send(`📢 \`|\` **${body.joke.length ? body.joke : 'Try again'}**`);
+	}catch(e){
+		return msg.channel.send(`Oh no an error occured :( \`${e.message}\` try again later`);
+	}
+}
+
+exports.conf = {
+  aliases: [],
+  clientPerm: '',
+  authorPerm: ''
+}
+
+exports.help = {
+  name: 'pun',
+  description: 'Generates a random pun.',
+  usage: 'pun',
+  example: ['pun']
+}
