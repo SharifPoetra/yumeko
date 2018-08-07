feccf1086641f70576de3c19137132471826d774
OwO <41247578+youKnowOwO@users.noreply.github.com>
2018-08-07 17:52:35 +0700

    editing member update and change some typo

--- commands/music/skip.js
+++ commands/music/skip.js
@@ -3,14 +3,15 @@ exports.run = async (client, msg, args) => {
 	if (!msg.member.voiceChannel) return msg.channel.send('You must join voice channel first');
 	if(serverQueue.voiceChannel.id !== msg.member.voiceChannel.id) return msg.channel.send(`You must be in **${serverQueue.voiceChannel.name}** to skip the song`);
 	if(!serverQueue) return msg.channel.send('Are you sure? nothing to skip because queue is empty');
-	if(serverQueue.songs[0].requester.id !== msg.author.id && serverQueue.voiceChannel.members.size > 2){
+	const members = serverQueue.voiceChannel.members.filter(x => !x.user.bot);
+	if(serverQueue.songs[0].requester.id !== msg.author.id && members.size > 2){
 		if(serverQueue.songs[0].votes.includes(msg.author.id)) return msg.channel.send('You already voted to skip this song');
 		serverQueue.songs[0].votes.push(msg.author.id);
 		if(serverQueue.songs[0].votes.length === 3){
 			msg.channel.send(`⏩ Skip the current song`);
 			return serverQueue.connection.dispatcher.end();
 		}
-		return msg.channel.send(`📢 You voted to skip this songs, need more votes! **${serverQueue.songs[0].votes.length} / 3`);
+		return msg.channel.send(`📢 You voted to skip this songs, need more votes! **${serverQueue.songs[0].votes.length} / 3**`);
 	}
 	msg.channel.send(`⏩ Skip the current song`);
 	return serverQueue.connection.dispatcher.end(); 
