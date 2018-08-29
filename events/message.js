1cef343527ee297047bf4af4cdb2879db3303943
OwO <41247578+youKnowOwO@users.noreply.github.com>
2018-08-30 01:49:02 +0700

    Update message.js

--- events/message.js
+++ events/message.js
@@ -4,4 +4,5 @@ module.exports = (client, msg) => {
 	if(msg.author.bot || !msg.guild) return;
 	if(msg.content.startsWith(PREFIX) || msg.content.startsWith(`${client.user.toString()} `)) return require('../handle/command.js')(client, msg);
   if((/```(js|javascript)(.|\s)+```/gi).test(msg.content)) return require('../handle/linter.js')(msg);
+	if((/```(cpp)(.|\s)+```/gi).test(msg.content)) return require('../handle/coliru.js')(msg);
 }
