0cd30987474340e0fb7cf268d41d609892d24d46
OwO <41247578+youKnowOwO@users.noreply.github.com>
2018-08-30 02:01:18 +0700

    Update coliru.js

--- handle/coliru.js
+++ handle/coliru.js
@@ -3,13 +3,6 @@ const { hastebin, codeblock } = require('./util.js');
 
 module.exports = async msg => {
 	const src = msg.content.match(/```(cpp)?(.|\s)+```/gi)[0].replace(/```(cpp)?|```/gi, '').trim();
-	let { text } = await snek.post('http://coliru.stacked-crooked.com/compile')
-	.send({
-		cmd: 'g++ main.cpp && ./a.out',
-		src
-	});
-	text = codeblock(text, 'diff');
-	if(text.length > 2000) text = await hastebin(text);
 	await msg.react('480697097629204492');
 	const filter = (rect, usr) => rect.emoji.id === '480697097629204492' && usr.id === msg.author.id;
 	const response = await msg.awaitReactions(filter,{
@@ -17,5 +10,16 @@ module.exports = async msg => {
 		time: 60000
 	});
 	if(!response.size) return undefined;
-	return msg.reply(text);
+	try {
+		let { text } = await snek.post('http://coliru.stacked-crooked.com/compile')
+		.send({
+			cmd: 'g++ main.cpp && ./a.out',
+			src
+		});
+		text = codeblock(text, 'diff');
+		if(text.length > 2000) text = await hastebin(text);
+		return msg.reply(text);
+	}catch (e){
+		return msg.reply(`Something went spoopy 👀 ${client.util.codeblock(e.stack, 'ini')}`);
+	}
 }
