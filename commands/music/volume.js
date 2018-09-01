8d6f77a24fc9fa5a4dc40c62f44a36a54619f3f6
OwO <41247578+youKnowOwO@users.noreply.github.com>
2018-09-01 14:47:28 +0700

    Create volume.js

---
+++ commands/music/volume.js
@@ -0,0 +1,30 @@
+exports.run = async (client, msg, args) => {
+	const serverQueue = client.queue.get(msg.guild.id);
+	if (!msg.member.voiceChannel) return msg.channel.send('You must join voice channel first');
+	if(serverQueue.voiceChannel.id !== msg.member.voiceChannel.id) return msg.channel.send(`You must be in **${serverQueue.voiceChannel.name}** to change the volume`);
+	if(!serverQueue) return msg.channel.send('Are you sure? queue is empty !');
+	try{
+		if(!args.length) return msg.channel.send(`🔈Current volume is ${serverQueue.volume}%`);
+		args[0].replace('%', '');
+		if(isNaN(args[0])) return msg.channel.send('Please input valid number >:(');
+		if(args[0] > 100) return msg.channel.send('Volume only can be set in range 1 - 100');
+		serverQueue.volume = args[0];
+		serverQueue.connection.dispatcher.setVolumeLogarithmic(args[1] / 50);
+		return msg.channel.send(`✅ Set volume to **${args[0]}**`);
+	}catch(e){
+		return msg.channel.send(`Oh no an error occured :( \`${e.message}\` try again later`);
+	}
+}
+
+exports.conf = {
+  aliases: [],
+  clientPerm: '',
+  authorPerm: ''
+}
+
+exports.help = {
+  name: 'volume',
+  description: 'change/show current queue volume',
+  usage: 'volume [number]',
+  example: ['volume', 'volume 100']
+}
