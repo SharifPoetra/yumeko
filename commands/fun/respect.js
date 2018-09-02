55bc3d58ec24c0b8f372b3d5f701fddc8e3d629c
OwO <41247578+youKnowOwO@users.noreply.github.com>
2018-09-02 21:00:50 +0700

    Create respect.js

---
+++ commands/fun/respect.js
@@ -0,0 +1,39 @@
+const { Attachment } = require('discord.js');
+const readFile = require('util').promisify(require('fs').readFile);
+const { Canvas } = require('canvas-constructor');
+
+exports.run = async (client, msg, args) => {
+	const user = msg.metions.users.first() || client.users.get(args[0]);
+	if(!user) user = msg.author;
+	try{
+		const paintMess = await msg.channel.send('🖌️ Painting...');
+		const plate = await readFile('./assets/images/image_respects.png');
+		const png = user.avatarURL.replace(/\.(gif|jpg|png|jpeg)\?size=(.+)/g, '.png?size=128');
+		const { body } = await client.snek.get(png);
+		const giveRespect = new Canvas(720, 405)
+		.addRect(0, 0, 720, 405)
+		.setColor('#000000')
+		.addImage(body, 110, 45, 90, 90)
+		.restore()
+		.addImage(plate, 0, 0, 720, 405)
+		.toBuffer();
+		await paintMess.delete();
+		return msg.channel.send(new Attachment(giveRespect, 'paid-respects.png'))
+		.then(x => x.react('🇫'));
+	}catch(e){
+		return msg.channel.send(`Oh no an error occured :( \`${e.message}\` try again later`);
+	}
+}
+
+exports.conf = {
+  aliases: ['pressf', 'f', 'rip', 'ripme'],
+  clientPerm: 'ATTACH_FILES',
+  authorPerm: ''
+}
+
+exports.help = {
+  name: 'respect',
+  description: 'Pay respects to someone.',
+  usage: 'respect [@user | id ]',
+  example: ['respect', 'respect @yumeko', 'respect 203733537282929']
+}
