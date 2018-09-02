ed45608f0488ab12c9ed38ce82576299bd6bd06e
OwO <41247578+youKnowOwO@users.noreply.github.com>
2018-09-03 06:36:31 +0700

    Create beautiful.js

---
+++ commands/fun/beautiful.js
@@ -0,0 +1,38 @@
+const { Attachment } = require('discord.js');
+const readFile = require('util').promisify(require('fs').readFile);
+const { Canvas } = require('canvas-constructor');
+
+exports.run = async (client, msg, args) => {
+	let user = msg.mentions.users.first() || client.users.get(args[0]);
+	if(!user) user = msg.author;
+	try{
+		const paintMess = await msg.channel.send('🖌️ Painting...');
+		const plate = await readFile('./assets/images/plate_beautiful.png');
+		const png = user.avatarURL.replace(/\.gif.+/g, '.png');
+		const { body } = await client.snek.get(png);
+		const getBeautiful = new Canvas(634, 675)
+		.setColor('#000000')
+		.addRect(0, 0, 634, 675)
+		.addImage(body, 423, 45, 168, 168)
+		.addImage(body, 426, 382, 168, 168)
+		.addImage(plate, 0, 0, 634, 675)
+		.toBuffer();
+		await paintMess.delete();
+		return msg.channel.send(new Attachment(getBeautiful, 'beautiful.jpg'));
+	}catch(e){
+		return msg.channel.send(`Oh no an error occured :( \`${e.message}\` try again later`);
+	}
+}
+
+exports.conf = {
+  aliases: [],
+  clientPerm: 'ATTACH_FILES',
+  authorPerm: ''
+}
+
+exports.help = {
+  name: 'beautiful',
+  description: 'Admire the beauty of another user or you',
+  usage: 'beautiful [@user | id ]',
+  example: ['beautiful', 'beautiful @yumeko', 'beautiful 203733537282929']
+}
