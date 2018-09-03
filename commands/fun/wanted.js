51ff61bb93c9ea22b4aa4559eee9fe37e46212fc
OwO <41247578+youKnowOwO@users.noreply.github.com>
2018-09-03 09:51:54 +0700

    Create wanted.js

---
+++ commands/fun/wanted.js
@@ -0,0 +1,37 @@
+const { Attachment } = require('discord.js');
+const readFile = require('util').promisify(require('fs').readFile);
+const { Canvas } = require('canvas-constructor');
+
+exports.run = async (client, msg, args) => {
+	let user = msg.mentions.users.first() || client.users.get(args[0]);
+	if(!user) user = msg.author;
+	try{
+		const paintMess = await msg.channel.send('🖌️ Painting...');
+		const plate = await readFile('./assets/images/plate_wanted.jpg');
+		const png = user.avatarURL.replace(/\.gif.+/g, '.png');
+		const { body } = await client.snek.get(png);
+		const getWanted = new Canvas(400, 562)
+		.setColor('#000000')
+		.addRect(0, 0, 400, 562)
+		.addImage(plate, 0, 0, 400, 562)
+		.addImage(body, 86, 178, 228, 228)
+		.toBuffer();
+		await paintMess.delete();
+		return msg.channel.send(new Attachment(getWanted, 'paid-respects.png'));
+	}catch(e){
+		return msg.channel.send(`Oh no an error occured :( \`${e.message}\` try again later`);
+	}
+}
+
+exports.conf = {
+  aliases: [],
+  clientPerm: 'ATTACH_FILES',
+  authorPerm: ''
+}
+
+exports.help = {
+  name: 'wanted',
+  description: 'Post a wanted picture of a user.',
+  usage: 'wanted [@user | id ]',
+  example: ['wanted', 'wanted @yumeko', 'wanted 203733537282929']
+}
