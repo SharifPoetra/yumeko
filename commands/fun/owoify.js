45e90f4768a4f88f4194e566bcd33926c1ee4201
OwO <41247578+youKnowOwO@users.noreply.github.com>
2018-09-04 15:32:18 +0700

    Update owoify.js

--- commands/fun/owoify.js
+++ commands/fun/owoify.js
@@ -3,7 +3,7 @@ const faces = ['(・`ω´・)', ';;w;;', 'owo', 'UwU', '>w<', '^w^'];
 exports.run = async (client, msg, args) => {
 	try{
 		args = args.join(' ') || 'What text want you to owoify?';
-		args.replace(/(?:r|l)/g, 'w')
+		args = args.replace(/(?:r|l)/g, 'w')
 		.replace(/(?:R|L)/g, 'W')
 		.replace(/n([aeiou])/g, 'ny$1')
 		.replace(/N([aeiou])/g, 'Ny$1')
