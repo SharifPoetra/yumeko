10740407b94b766718bc3f2415a1119ce68d256b
OwO <41247578+youKnowOwO@users.noreply.github.com>
2018-09-06 14:53:40 +0700

    Create sepia.js

---
+++ commands/fun/sepia.js
@@ -0,0 +1,37 @@
+const { Attachment } = require('discord.js');
+const { Canvas, sepia } = require('canvas-constructor');
+const { loadImage } = require('canvas');
+
+exports.run = async (client, msg, args) => {
+	let user = msg.mentions.users.first() || client.users.get(args[0]);
+	if(!user) user = msg.author;
+	try{
+		const paintMess = await msg.channel.send('🖌️ Painting...');
+		const png = user.avatarURL.replace(/\.gif/g, '.png');
+		const { body } = await client.snek.get(png);
+		const avatar = loadImage(body);
+		let newSepia = new Canvas(avatar.width, avatar.height)
+		.addRect(0, 0, avatar.width, avatar.height)
+		.setColor('#000000')
+		.addImage(body, 0, 0, avatar.width, avatar.height);
+		newSepia = sepia(newSepia)
+		.toBuffer();
+		await paintMess.delete();
+		return msg.channel.send(new Attachment(newSepia, 'sepia.png'));
+	}catch(e){
+		return msg.channel.send(`Oh no an error occured :( \`${e.message}\` try again later`);
+	}
+}
+
+exports.conf = {
+  aliases: [];
+  clientPerm: 'ATTACH_FILES',
+  authorPerm: ''
+}
+
+exports.help = {
+  name: 'sepia',
+  description: 'Draws an user\'s avatar in sepia.',
+  usage: 'sepia [@user | id ]',
+  example: ['sepia', 'sepia @yumeko', 'sepia 203733537282929']
+}
